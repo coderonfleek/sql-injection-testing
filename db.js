adb37d5c1b028878db5965e602615cf1773b1c5e
Fikayo <fik4christ@yahoo.com>
2021-01-28 01:16:44 +0100

    Base Project

--- db.js
+++ db.js
@@ -1,11 +1,4 @@
-/* const mysql = require("mysql");
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || "root",
-    password : process.env.DB_PASSWORD || "",
-    database : process.env.DB_NAME || "injection_test"
-}); */
 const sqlite3 = require("sqlite3");
 
 const db = new sqlite3.Database(':memory:');
@@ -17,10 +10,5 @@ let sql = "CREATE TABLE IF NOT EXISTS `users` ( `id` INTEGER PRIMARY KEY AUTOINC
 db.run(sql)
 
 
-/* db.query(sql, function (err, data, fields) {
-    if(err) throw err
-    
-    console.log(fields);
-}) */
 
 module.exports = db;
--- user.js
+++ user.js
@@ -17,9 +17,9 @@ function getUsers(req, res) {
 
 function getUser(req, res) {
 
-    let sql = `SELECT * FROM users WHERE id=?`;
+    let sql = `SELECT * FROM users WHERE id='${req.body.id}'`;
 
-    db.all(sql, [req.body.id], function (err, data) {
+    db.all(sql, function (err, data) {
         if(err) throw err
         
         res.json({
